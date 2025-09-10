357994cc4f2e64be53afd866cfec2878cf8d6192
Romulo Nogueira <romulonogueira1607@gmail.com>
2025-09-09 21:24:18 -0300

    implementado o metodo put bem como validações de campos com preenchimento obrigatorio

--- Projetos/01 - api-conexao-array/src/routes/routes.js
+++ Projetos/01 - api-conexao-array/src/routes/routes.js
@@ -4,8 +4,25 @@ const routes = express.Router();
 
 // Construindo array de armazenamento de dados
 
-const filme = [];
+const filmes = [];
 
+routes.get('/filmes', (req, res) =>{
+       
+    res.json(filmes);
+    res.status(200).send("Filme visualizado com sucesso! ")
+})
+
+routes.get('/filmes/:id', (req, res) => {
+    const id = req.params.id;
+    // O find tem a lógica de procurar o primeiro elemento que satisfaça a condição
+    const filme = filmes.find(item => item.id == id);
+
+    if(!filme){
+        return res.status(404).send("Erro 404 - Filme não encontrado")
+    }
+
+    res.status(200).send(` O filme encontrado foi: ${filme.id} - ${filme.titulo} `)
+})
 
 // criando dados do filme
 
@@ -13,14 +30,39 @@ routes.post('/filmes', (req, res) => {
    /* const id = req.body.id;  --- caso feito assim deveria fazer de 1 por 1 */
 
    const {id, titulo, ano, categoria} = req.body;
-   filme.push({id, titulo, ano, categoria});
+
+   //Validação de campos Obrigatorios
+
+   if(!id || !titulo || !ano || !categoria || id.toString().trim() == "" || titulo.trim() == "" || ano.toString().trim() == "" || categoria.trim() == ""){
+        return res.status(400).send('Os campos não podem ser vazios')
+   }
+
+    
+
+   filmes.push({id, titulo, ano, categoria});
    res.status(201).send("Filme adicionado com sucesso! ")
 })
 
-routes.get('/fimes/cadastrados', (req, res) =>{
-       
-    res.json(filme);
-    res.status(200).send("Filme visualizado com sucesso! ")
+//Criando método de atualizacao
+
+routes.put('/filmes/:id', (req, res) =>{
+    /* const id = req.params.id; */ //
+    const {id} = req.params
+    const {titulo, ano, categoria} = req.body;
+    const updateFilme = filmes.find(item => item.id);
+
+    if(!updateFilme){
+        return res.status(404).send('Erro 404 - Filme não encontrado')
+    }
+    if(!titulo || titulo.trim() ===""){
+        res.status(400).send('Titulo não pode ser vazio')
+    }
+
+    updateFilme.titulo = titulo;
+    updateFilme.ano = ano;
+    updateFilme.categoria = categoria;
+
+    res.status(200).send('Filme atualizado com sucesso!')
 })
 
 export default routes;

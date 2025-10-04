17a6fcdf477bac3d99a2abc442c3dfba3ec67a42
Romulo Nogueira <romulonogueira1607@gmail.com>
2025-10-03 21:15:01 -0300

    finalizando a api

--- Projetos/03-api-list-task/src/controller/tarefaController.js
+++ Projetos/03-api-list-task/src/controller/tarefaController.js
@@ -25,6 +25,44 @@ class TarefaController {
             
         }
     }
+
+    // Buscando ID
+
+    static async BuscarId(req, res){
+        try {
+            const {id} =req.params;
+            const tarefas = await tarefa.findById(id);
+            if(!tarefas) return res.status(404).json({erro: "Id da tarefa não encontrado!"})
+            res.status(200).json({mensagem: `Id encontrado: ${tarefas}`})
+        } catch (error) {
+            res.status(500).json({mensagem:`Falha na busca pelo ID da tarefas - ${error}`})
+        }
+    }
+
+    // autalizar tarefa
+
+    static async AtualizarTarefa(req,res){
+        try {
+            const {id} = req.params;
+            await tarefa.findByIdAndUpdate(id, req.body);
+            res.status(200).json({mensagem:"Tarefa atualizada com sucesso!" })
+        } catch (error) {
+            res.status(500).json({mensagem: `Erro ao atualizar tarefa - ${error}`});
+        }
+    }
+
+    static async DeletarTarefa(req, res){
+        try {
+            const {id} = req.params;
+            const deletar = await tarefa.findByIdAndDelete(id);
+            if(!deletar){
+                res.status(404).json({mensagem: "Tarefa não encontrada"})
+            }
+            res.status(200).json({mensagem: "Tarefa deletada com sucesso!"});
+        } catch (error) {
+             res.status(500).json({mensagem: `Erro ao deletar tarefa - ${error}`})
+        }
+    }
 }
 
 export default TarefaController;
--- Projetos/03-api-list-task/src/routes/taskRoutes.js
+++ Projetos/03-api-list-task/src/routes/taskRoutes.js
@@ -5,5 +5,7 @@ const routesTask = express.Router();
 
 routesTask.get('/tarefas', TarefaController.exibirTarefa);
 routesTask.post('/tarefas', TarefaController.CriarTarefas);
-
+routesTask.get('/tarefas/:id', TarefaController.BuscarId);
+routesTask.put('/tarefas/:id', TarefaController.AtualizarTarefa);
+routesTask.delete('/tarefas/:id', TarefaController.DeletarTarefa)
 export default routesTask;
